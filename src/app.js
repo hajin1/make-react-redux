319ffa9a1038882cc90b0a72ff8b158c300da2d4
hajin <hajinkim@kangwon.ac.kr>
2022-12-12 22:31:38 +0900

    2-6 함수형 컴포넌트

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 /* @jsx createElement */
 import { render, createElement } from './react';
 
+function Title(props) {
+	return <h1>{props.children}</h1>;
+}
+
+function Item(props) {
+	return <li style={`color:${props.color}`}>{props.children}</li>;
+}
+
 const vdom = (
 	<p>
-		<h1>React 만들기</h1>
+		<Title label="haha">React 잘 만들기</Title>
 		<ul>
-			<li style="color: red">첫 번째 아이템</li>
-			<li style="color:blue">두 번째 아이템</li>
-			<li style="color: green">세 번째 아이템</li>
+			<Item color="red">첫 번째 아이템</Item>
+			<Item color="blue">두 번째 아이템</Item>
+			<Item color="green">세 번째 아이템</Item>
 		</ul>
 	</p>
 );
